41fb8eeabfcf6863f3d29234ca0078b30bdab341
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-08-18 20:54:11 -0700

    Standard Input/Output

---
+++ lesson1Standardio.java
@@ -0,0 +1,24 @@
+package Day1;
+
+//program ဆိုသည်မှာ set of instruction ဖြစ်တယ်။ instruction တွေသည် line by line သွားသည်။
+//program တွေကို statement တွေကိုသုံးရေးတယ်။ Programming language တွေသည် ရေးထုံးရေးနည်း ပုံစံတစ်ခုနဲ့တစ်ခုသည် မတူပါ။
+//programming တွေသည် ရေးပုံရေးနည်းသာ မတူပေမဲ့ တွေးရမည့်logic သည် တူတူပင်ဖြစ်သည်။
+//lesson1standario ဆိုတာသည် class တစ်ခု၏ nameဖြစ်သည်။
+//class တစ်ခု၏ name နှင့် file ၏ name နှင့်တူရမည်။
+//class များကို အမည်ပေးရာတွင် ရှေ့ဆုံးစာလုံးအကြီးဖြင့်ပေးရမည်။
+//Case Sensitivity
+//.java extension ဖြင့်ဆုံးသည်။
+public class lesson1ှှStandardio {
+
+    //main method ဖြစ်သည်။
+    //java compiler သည် code များကို main method မှသာ စတင် execute လုပ်ပေးသည်။
+    //main သည် Java application တစ်ခု၏ entry စမှတ် ဝင်ပေါက် တစ်ခုဖြစ်သည်။
+    public static void main(String args[])
+    {
+        //program သည် main ကနေမှ စအလုပ်လုပ်သည်။
+        System.out.println("Hello, World");//Statement တစ်ကြောင်းပြီးတိုင်း semi-comma နဲ့ အဆုံးသတ်သည်။
+    }
+
+}
+
+
