15161b09b1d59bdc8f0ab9401b373262c6755877
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-08-18 20:58:32 -0700

    Create lesson5Variable.java

---
+++ lesson5Variable.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class lesson5Variable {
+    int data=50;//instance variable
+    static int m=100;//static variable
+    void method(){
+        int n=90;//local variable
+    }
+    public static void main(String args[])
+    {
+        //program သည် main ကနေမှ စအလုပ်လုပ်သည်။
+        System.out.println("Hello, World");
+    }
+}
