68cd7acbdf9b1908e154d518b50a88726c7c0613
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-08-18 21:09:49 -0700

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,32 @@
+package ncc;
+
+import java.util.Scanner;
+
+public class pattern1{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int i = input.nextInt();
+        System.out.println("Please enter a number: ");
+        int j = input.nextInt();
+        int total = i+j;
+        System.out.println("Your total number is: "+total);
+
+        for(int k=0;k<=total;k++){
+            for(int x=total-k;x>0;x--){
+                System.out.print(" ");
+            }
+            for(int z=0;z<=k;z++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+//           *
+//          * *
+//         * * *
+//        * * * *
+//       * * * * *
+//      * * * * * *
+}
+
